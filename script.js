8b156171ad22ecc44ac50cff7f265b0a30f9ad4e
Surajr36 <surajragu@gmail.com>
2021-09-19 12:40:01 +0530

    New

---
+++ script.js
@@ -0,0 +1,40 @@
+// // let js = "amazing";
+// // console.log(40 + 8 + 23 - 10);
+
+// // console.log("Suraj");
+// // let PI = 3.1414;
+// // console.log(PI);
+
+// let country = "India";
+// console.log(country);
+
+// // let continent = "Asia";
+// // console.log(continent);
+
+// let pop = 1300;
+// // console.log(pop);
+
+// // let dumbVar = true;
+// // console.log(dumbVar);
+
+// // console.log(typeof dumbVar);
+
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof pop);
+// console.log(typeof country);
+// console.log(typeof language);
+
+let markWeight = 95;
+let johnWeight = 85;
+
+let markHeight = 1.88;
+let johnHeight = 1.76;
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / (johnHeight * johnHeight);
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
