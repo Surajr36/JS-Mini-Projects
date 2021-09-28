2327ce645441c49d68a7f8042e08c9e0eef76c54
Surajr36 <surajragu@gmail.com>
2021-09-28 23:09:30 +0530

    Incremental work done on Bankist

--- JS Mini Projects/Bankist/script.js
+++ JS Mini Projects/Bankist/script.js
@@ -2,6 +2,68 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+// LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(key, value);
+//   // console.log(map);
+// });
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// console.log(arr.slice(1, -2));
+// movements.forEach(function (movement) {
+//   if (movement > 0) console.log(`You deposited ${movement}`);
+//   else console.log(`You withdrew ${Math.abs(movement)}`);
+// });
+
+// function checkDogs(dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, index) {
+//     console.log(
+//       `Dog number ${index + 1} is ${
+//         dog >= 3
+//           ? 'is an adult and is ' + dog + ' years old🐕‍🦺'
+//           : 'still a puppy🐶'
+//       }`
+//     );
+//   });
+// }
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// console.log(accounts);
+
+// //(Using Filters)
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// const withdrawals = movements.filter(mov => mov < 0);
+
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   return acc + cur;
+// }, 0);
+
+//REDUCE
+// const maxValue = movements.reduce(function (acc, mov) {
+//   if (mov > acc) return mov;
+//   else return acc;
+// }, movements[0]);
+// console.log(maxValue);
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -61,28 +123,43 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//Getting the movements to work using DOM Manipulation
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
 
-/////////////////////////////////////////////////
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
 
-// currencies.forEach(function (value, key, map) {
-//   console.log(key, value);
-//   // console.log(map);
-// });
-// let arr = ['a', 'b', 'c', 'd', 'e'];
+//Computing usernames. Here username is just the initials(Using map and forEach)
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsername(accounts);
 
-// console.log(arr.slice(1, -2));
-// movements.forEach(function (movement) {
-//   if (movement > 0) console.log(`You deposited ${movement}`);
-//   else console.log(`You withdrew ${Math.abs(movement)}`);
-// });
+//Used to manipulate balance in main page using reduce method
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce(function (acc, mov) {
+    return acc + mov;
+  }, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
