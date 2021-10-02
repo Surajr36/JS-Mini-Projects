46b19987ee21f59f24535f5aa84826de124723c6
Surajr36 <surajragu@gmail.com>
2021-10-02 22:53:59 +0530

    Bankist website scrolling implemented

--- JS Mini Projects/Bankist Website/script.js
+++ JS Mini Projects/Bankist Website/script.js
@@ -1,4 +1,19 @@
 'use strict';
+//////////////OLD METHOD OF DOING SCROLLING////////////////
+// const s1coords = section1.getBoundingClientRect();
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
+////////////// SAMPLE EVENTS ////////////
+// const h1 = document.querySelector('h1');
+// const alertH1 = function (e) {
+//   alert('addEventListener:Great! You are reading');
+// };
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 ///////////////////////////////////////
 // Modal window
@@ -8,7 +23,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault(); //To prevent the page from jumping up when the button is clicked
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +34,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +43,17 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+//USING btnScrollTo to direct Learn more to section 1
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Implementing page navigation
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+  });
 });
