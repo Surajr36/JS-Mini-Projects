891e5c3665b483773d085c46855924656876b257
Surajr36 <surajragu@gmail.com>
2021-10-03 17:07:03 +0530

    Lil changes

--- JS Mini Projects/Bankist Website/script.js
+++ JS Mini Projects/Bankist Website/script.js
@@ -15,6 +15,15 @@
 
 // setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
+//Implementing page navigation
+//Inefficient code
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = document.querySelector(this.getAttribute('href'));
+//     id.scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 ///////////////////////////////////////
 // Modal window
 
@@ -52,8 +61,68 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 //Implementing page navigation
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-  });
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = document.querySelector(e.target.getAttribute('href'));
+    id.scrollIntoView({ behavior: 'smooth' });
+  }
 });
+
+//Building Tabbed Component(Using event delegation)
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause just in case somewhere in the tabs container clicked and closest returns NULL
+  if (!clicked) return;
+
+  //All are removed at first then only the clicked one is added
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//Menu fade animations
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
+});
+nav.addEventListener('mouseout', function (e) {
+  handleHover(e, 1);
+});
+
+//Sticky nav
+//OLD WAY
+const initialCoOrds = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoOrds.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
+//USING INTERSECTION OBSERVER API
+const obsCallback = function () {};
+const observeOptions = {
+  root: null,
+};
+const observer = new IntersectionObserver();
+observer.observe(section1);
