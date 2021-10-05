708f7051abb5c4860568dc812c7b999f02457c30
Surajr36 <surajragu@gmail.com>
2021-10-06 00:11:47 +0530

    Mapty project start

--- JS Mini Projects/Bankist Website/script.js
+++ JS Mini Projects/Bankist Website/script.js
@@ -186,12 +186,23 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 //Implementing slider
+
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
 let curSlide = 0;
 const maxSlide = slides.length;
 
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+// activateDot(1);
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
@@ -202,12 +213,41 @@ const nextSlide = function () {
   if (curSlide === maxSlide - 1) curSlide = 0;
   else curSlide++;
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 const prevSlide = function () {
   if (curSlide === 0) curSlide = maxSlide - 1;
   else curSlide--;
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
+
+//Implementing keyboard functionality of slider
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+});
+
+//Implementing DOTS
+const dotContainer = document.querySelector('.dots');
+
+const createDots = function () {
+  slides.forEach(function (s, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+createDots();
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const slide = e.target.dataset.slide;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
