8f0e1df062c0d06a5bfe956ea1f178ba1941e2a1
Surajr36 <surajragu@gmail.com>
2021-10-22 20:15:23 +0530

    Project added

---
+++ PP/src/Components/Users/AddUser.js
@@ -0,0 +1,34 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+import { useState } from "react";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
