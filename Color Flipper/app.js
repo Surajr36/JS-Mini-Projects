60846c335648a727355bda3e93d213609071aabf
Surajr36 <surajragu@gmail.com>
2021-11-17 13:48:52 +0530

    New Project added

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -1,1 +1,26 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [
+  "black",
+  "white",
+  "gray",
+  "silver",
+  "maroon",
+  "red",
+  "purple",
+  "fushsia",
+  "green",
+  "lime",
+  "olive",
+  "yellow",
+  "navy",
+  "blue",
+  "teal",
+  "aqua",
+];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
