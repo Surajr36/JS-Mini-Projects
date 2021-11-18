743aafb07ea8f1903fa677ec9429b97198759930
Surajr36 <surajragu@gmail.com>
2021-11-18 14:37:23 +0530

    Changes

--- Menu bar/app.js
+++ Menu bar/app.js
@@ -71,4 +71,71 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "Steak dinner",
+    category: "dinner",
+    price: 31.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+// let filterBtns;
+const btnContainer = document.querySelector(".btn-container");
+// console.log(btnContainer);
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+              <img src="${item.img}" alt="${item.title}" class="photo" />
+              <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+               </header>
+               <p class="item-text">
+               ${item.desc}
+                </p>
+              </div>
+            </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) values.push(item.category);
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-id="${category}">
+            ${category}
+            </button>`;
+    })
+    .join("");
+  btnContainer.innerHTML = categoryBtns;
+
+  // Selecting buttons after adding them dynamically
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  filterBtns.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) return menuItem;
+      });
+      if (category === "all") displayMenuItems(menu);
+      else displayMenuItems(menuCategory);
+    });
+  });
+}
